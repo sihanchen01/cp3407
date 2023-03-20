ea9b5d1cb0bdb09e57509066a5d753dff41486ee
sihanchen <sihanchen01@gmail.com>
2023-03-20 13:40:43 +0800

    about page style

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -1,9 +1,12 @@
 const About = () => {
   return(
     <>
-    <h4>Welcome to our website, where we help you unlock your creative potential by generating ideas from your prompts. Our platform is designed to help you overcome creative blocks and spark new ideas for any project or task you may have.</h4>
-    <h4>Our team of AI-powered algorithms is dedicated to providing you with the best ideas possible. All you need to do is simply enter your prompt, and our system will generate a list of unique and innovative ideas tailored to your needs.</h4>
-    <h4>Our platform is perfect for anyone looking to jumpstart their creativity, whether you're a writer, an artist, a marketer, or just someone looking for inspiration for a personal project. We believe that everyone has the potential to be creative, and we're here to help you unlock your full creative potential.</h4>
+    <div className="container">
+
+      <h4>Welcome to our website, where we help you unlock your creative potential by generating ideas from your prompts. Our platform is designed to help you overcome creative blocks and spark new ideas for any project or task you may have.</h4>
+      <h4>Our team of AI-powered algorithms is dedicated to providing you with the best ideas possible. All you need to do is simply enter your prompt, and our system will generate a list of unique and innovative ideas tailored to your needs.</h4>
+      <h4>Our platform is perfect for anyone looking to jumpstart their creativity, whether you're a writer, an artist, a marketer, or just someone looking for inspiration for a personal project. We believe that everyone has the potential to be creative, and we're here to help you unlock your full creative potential.</h4>
+    </div>
     </>
   )
 }
